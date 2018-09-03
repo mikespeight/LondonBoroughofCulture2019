f8405d971aa8c47301c47b91d0ef1b1acd444a35
Mike Speight <speight.mike@gmail.com>
2018-09-03 10:36:51 +0100

    First content row added with assets

--- assets/js/BoC.js
+++ assets/js/BoC.js
@@ -4,3 +4,29 @@
 
 "use strict";
 
+
+//countdown
+function getTimeRemaining(endtime){
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor( (t/1000) % 60 );
+	var minutes = Math.floor( (t/1000/60) % 60 );
+	var hours = Math.floor( (t/(1000*60*60)) % 24 );
+	var days = Math.floor( t/(1000*60*60*24) );
+	return {
+		'total': t,
+		'days': days,
+		'hours': hours,
+		'minutes': minutes,
+		'seconds': seconds
+	};
+}
+
+
+$(document).ready(function(){
+
+	var deadline = '2019-01-01', //yyy-mm-dd
+		$count = $('#count');
+
+	$count.html(getTimeRemaining(deadline).days + 1);
+
+});
